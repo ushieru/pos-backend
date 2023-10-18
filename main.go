a796f43c24231d86f2aca72c414617f43ceb10a9
ushieru <uzielcocolan@gmail.com>
2023-10-17 18:20:04 -0600

    Feat: Init CLI

--- main.go
+++ main.go
@@ -1,41 +1,7 @@
 package main
 
-import (
-	"log"
+import "github.com/ushieru/pos/cmd"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/swagger"
-	"github.com/ushieru/pos/database"
-	"github.com/ushieru/pos/routes"
-	"github.com/ushieru/pos/routes/api"
-	api_v1 "github.com/ushieru/pos/routes/api/v1"
-
-	_ "github.com/ushieru/pos/docs"
-)
-
-// @Title Point Of Sale API
-// @Version 1.0
-// @Description Point Of Sale - Total Tools
-// @Host localhost:8080
-// @SecurityDefinitions.apikey ApiKeyAuth
-// @In header
-// @Name Authorization
-// @BasePath /
 func main() {
-	app := fiber.New()
-	database.InitDatabase()
-	setupRoutes(app)
-	log.Fatal(app.Listen(":8080"))
-}
-
-func setupRoutes(app *fiber.App) {
-	app.Use(cors.New())
-
-	app.Static("/", "./public") 
-	app.Get("/swagger/*", swagger.HandlerDefault)
-	app.Get("/ping", api.GetPingRequest)
-	api_v1.SetupApiV1(app)
-	
-	app.Use(routes.RouteNotFound)
+	main_cmd.NewCLI().Execute()
 }
