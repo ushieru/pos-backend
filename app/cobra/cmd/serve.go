b4dc7b6bf202e989623e35b8ed49bce2bd5e59c7
ushieru <uzielcocolan@gmail.com>
2024-05-30 23:53:31 -0600

    Refactor: delete uber/fx

--- app/cobra/cmd/serve.go
+++ app/cobra/cmd/serve.go
@@ -1,80 +1,43 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/ushieru/pos/app/fiber"
-	handler "github.com/ushieru/pos/app/fiber/handlers"
-	"github.com/ushieru/pos/app/fiber/middlewares"
 	"github.com/ushieru/pos/db"
-	"github.com/ushieru/pos/domain"
 	"github.com/ushieru/pos/repository"
 	"github.com/ushieru/pos/service"
-	"go.uber.org/fx"
 )
 
 var ServeCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Starts the web server",
 	Run: func(command *cobra.Command, args []string) {
-		fx.New(
-			fx.Provide(
-				getConfig,
-				newFiberApp,
-				db.GetDB,
-				fx.Annotate(repository.NewUserGormRepository, fx.As(new(domain.IUserRepository))),
-				fx.Annotate(
-					repository.NewCategoryGormRepository,
-					fx.As(new(domain.ICategoryRepository)),
-				),
-				fx.Annotate(
-					repository.NewProductGormRepository,
-					fx.As(new(domain.IProductRepository)),
-				),
-				fx.Annotate(
-					repository.NewTicketGormRepository,
-					fx.As(new(domain.ITicketRepository)),
-				),
-				fx.Annotate(repository.NewTableGormRepository, fx.As(new(domain.ITableRepository))),
-				fx.Annotate(service.NewUserService, fx.As(new(service.IUserService))),
-				fx.Annotate(service.NewCategoryService, fx.As(new(service.ICategoryService))),
-				fx.Annotate(service.NewProductService, fx.As(new(service.IProductService))),
-				fx.Annotate(service.NewTicketService, fx.As(new(service.ITicketService))),
-				fx.Annotate(service.NewTableService, fx.As(new(service.ITableService))),
-			),
-			fx.Invoke(
-				fiber_app.Init,
-				handler.NewPingHandler,
-				handler.NewSwaggerHandler,
-				handler.NewAuthHandler,
-				middlewares.NewAuthMiddleware,
-				handler.NewUserHandler,
-				handler.NewCategoryHandler,
-				handler.NewProductHandler,
-				handler.NewTicketHandler,
-				handler.NewTableHandler,
-				handler.NewNotFoundHandler,
-			),
-			fx.NopLogger,
-		).Run()
-	},
-}
+		config := getConfig()
+		fiberApp := fiber_app.NewFiberApp(config)
+		database := db.GetDB(config.DatabaseLogger, config.DatabaseName)
+		userRepository := repository.NewUserGormRepository(database)
+		categoryRepository := repository.NewCategoryGormRepository(database)
+		productRepository := repository.NewProductGormRepository(database)
+		ticketRepository := repository.NewTicketGormRepository(database)
+		tableRepository := repository.NewTableGormRepository(database)
 
-func newFiberApp(lc fx.Lifecycle, c *fiber_app.FiberAppConfig) *fiber_app.FiberApp {
-	fiberApp := fiber_app.NewFiberApp(c)
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			go fiberApp.App.Listen(fmt.Sprintf(":%d", c.Port))
-			return nil
-		},
-		OnStop: func(ctx context.Context) error {
-			return fiberApp.Stop()
-		},
-	})
-	return fiberApp
+		userService := service.NewUserService(userRepository)
+		categoryService := service.NewCategoryService(categoryRepository)
+		productService := service.NewProductService(productRepository, categoryRepository)
+		tableService := service.NewTableService(tableRepository)
+		ticketService := service.NewTicketService(ticketRepository, tableRepository)
+
+		fiberApp.Init(&fiber_app.FiberAppServices{
+			UserService:     userService,
+			CategoryService: categoryService,
+			ProductService:  productService,
+			TableService:    tableService,
+			TicketService:   ticketService,
+		})
+	},
 }
 
 func getConfig() *fiber_app.FiberAppConfig {
